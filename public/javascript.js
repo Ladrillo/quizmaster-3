49fa996bd7f05546fb35e545017744c6c1c9de00
Ladrillo <Gabriel@Gabriels-Mac-Pro.local>
2015-05-18 16:23:44 +0200

    retoques fce-08 barron 04

--- public/javascript.js
+++ public/javascript.js
@@ -1,8 +1,21 @@
 $(document).ready(function () {
-	$('div.gabri-button').mouseenter(function () {
-        $('div.gabri-button').fadeTo('fast', 0.65);
+	
+    $('div.gabri-button').click(function () {
+        $(this).effect('explode');    
     });
-    $('div.gabri-button').mouseleave(function () {
-        $('div.gabri-button').fadeTo('fast', 1);
-    });
+
+
+    // $('.logo-text').html('Hello World!');        
+    // $('div').css('background-color', 'red');
+    // var paragraphs = $('h3');
+    // alert(paragraphs.length);
+    // $('h3').each(function () { alert($(this).html()); });
+    // $('input[type="text"]').css('background-color', 'yellow');
+    // alert("This pops once everything is loaded!")
+    // $('div.gabri-button').mouseenter(function () {
+    //     $('div.gabri-button').fadeTo('fast', 0.65);
+    // });
+    // $('div.gabri-button').mouseleave(function () {
+    //     $('div.gabri-button').fadeTo('fast', 1);
+    // }); 
 })
